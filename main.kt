95a2c52dcf1351102fca0ae54461bb5d60ea08d0
dunow <dunow33@hotmail.com>
2021-07-21 18:44:59 -0400

    more kotlin practice

--- main.kt
+++ main.kt
@@ -49,3 +49,42 @@ fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Bool
     }
 }
 
+/*
+import java.util.*;
+
+fun main(args: Array<String>) {
+    feedTheFish()
+}
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" -> true
+        else -> false
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+} */
