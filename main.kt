76845b75e7faa10d9b1a31c17a799b5b8c7ee44e
dunow <dunow33@hotmail.com>
2021-07-27 03:36:33 -0400

    Kotlin functional variable practice

--- main.kt
+++ main.kt
@@ -160,3 +160,22 @@ fun main(){
     val list = listOf("Kotlin", "Java", "C++", "C#")
     printFilteredStrings(list, { it.startsWith("K")})
 }
+
+fun printFilteredStrings(list: List<String>, predicate: ((String) -> Boolean)?){
+    list.forEach{
+        if(predicate?.invoke(it) == true){
+            println(it)
+        }
+    }
+}
+
+val predicate: (String) -> Boolean = {
+    it.startsWith("J")
+}
+
+fun main(){
+    val list = listOf("Kotlin", "Java", "C++", "C#")
+    printFilteredStrings(list, predicate)
+
+    printFilteredStrings(list, null)
+}
